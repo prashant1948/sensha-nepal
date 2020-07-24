9e91ef4bf841f080c36a171ba73f0e80bdaeb67e
Prashant Thapa <thapa.prashant566@gmail.com>
2020-07-24 15:52:22 +0545

    Update

--- services.php
+++ services.php
@@ -416,7 +416,24 @@
             </div>
         </footer>
     </body>
-</html></div><!-- Vendor --><script src="vendor/jquery/jquery.min.js"></script><script src="vendor/jquery.appear/jquery.appear.min.js"></script><script src="vendor/jquery.easing/jquery.easing.min.js"></script><script src="vendor/jquery.cookie/jquery.cookie.min.js"></script><script src="master/style-switcher/style.switcher.js" id="styleSwitcherScript" data-base-path="" data-skin-src=""></script><script src="vendor/popper/umd/popper.min.js"></script><script src="vendor/bootstrap/js/bootstrap.min.js"></script><script src="vendor/common/common.min.js"></script><script src="vendor/jquery.validation/jquery.validate.min.js"></script><script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script><script src="vendor/jquery.gmap/jquery.gmap.min.js"></script><script src="vendor/jquery.lazyload/jquery.lazyload.min.js"></script><script src="vendor/isotope/jquery.isotope.min.js"></script><script src="vendor/owl.carousel/owl.carousel.min.js"></script><script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script><script src="vendor/vide/jquery.vide.min.js"></script><script src="vendor/vivus/vivus.min.js"></script><!-- Theme Base, Components and Settings --><script src="js/theme.js"></script><!-- Current Page Vendor and Views --><script src="vendor/rs-plugin/js/jquery.themepunch.tools.min.js"></script><script src="vendor/rs-plugin/js/jquery.themepunch.revolution.min.js"></script><script src="vendor/circle-flip-slideshow/js/jquery.flipshow.min.js"></script><script src="js/views/view.home.js"></script><!-- Theme Initialization Files --><script src="js/theme.init.js"></script><script>
+</html></div>
+<!-- Vendor --><script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/jquery.appear/jquery.appear.min.js">
+</script><script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+<script src="vendor/jquery.cookie/jquery.cookie.min.js"></script>
+<script src="master/style-switcher/style.switcher.js" id="styleSwitcherScript" data-base-path="" data-skin-src=""></script>
+<script src="vendor/popper/umd/popper.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="vendor/common/common.min.js"></script>
+<script src="vendor/jquery.validation/jquery.validate.min.js"></script>
+<script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
+<script src="vendor/jquery.lazyload/jquery.lazyload.min.js"></script>
+<script src="vendor/isotope/jquery.isotope.min.js"></script>
+<script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="vendor/vide/jquery.vide.min.js"></script>
+<script src="vendor/vivus/vivus.min.js"></script><!-- Theme Base, Components and Settings --><script src="js/theme.js"></script><!-- Current Page Vendor and Views --><script src="vendor/rs-plugin/js/jquery.themepunch.tools.min.js"></script><script src="vendor/rs-plugin/js/jquery.themepunch.revolution.min.js"></script><script src="vendor/circle-flip-slideshow/js/jquery.flipshow.min.js"></script><script src="js/views/view.home.js"></script><!-- Theme Initialization Files --><script src="js/theme.init.js"></script><script>
 (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
     i[r] = i[r] || function () {
